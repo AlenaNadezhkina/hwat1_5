dffeccab1875eed58a942ded749bf47caa99e85f
AlenaNadezhkina <61390359+AlenaNadezhkina@users.noreply.github.com>
2025-08-05 20:09:43 +0300

    Add branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldReturn100() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn300() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1700;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 300;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1200() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = -200;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1200;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
